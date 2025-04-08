ce5c27bd2cda4210dc114789bcbf44b002165c76
Chatan Petry <95079984+Jesselpetry@users.noreply.github.com>
2025-04-09 05:17:17 +0700

    Menu Fix

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -37,7 +37,7 @@ export default function Menu() {
     const pathname = usePathname();
     
     return (
-        <div className="fixed bottom-5 left-0 right-0 w-full flex justify-center px-4 pb-2 z-10 max-w-screen-lg mx-auto">
+        <div className="fixed bottom-5 left-0 right-0 w-full flex justify-center px-4 pb-2 z-10 max-w-screen mx-auto">
             <div className="w-full max-w-xl p-5 rounded-3xl bg-black/25 backdrop-blur-sm border border-white/30 text-white text-lg font-light shadow-lg shadow-black/20">
             <div className="flex items-center justify-between w-full">
                 {navItems.map((item, index) => {
@@ -49,7 +49,7 @@ export default function Menu() {
                         href={`/${item.path}`} 
                         className='flex flex-col items-center transition-all duration-200 hover:scale-110 group'
                     >
-                        <div className={`text-l flex flex-col items-center justify-center text-center ${isActive ? 'drop-shadow-lg' : 'opacity-50'} transition-all duration-200 group-hover:opacity-100 group-hover:drop-shadow-md space-y-2`}>
+                        <div className={`text-sm flex flex-col items-center justify-center text-center ${isActive ? 'drop-shadow-lg' : 'opacity-50'} transition-all duration-200 group-hover:opacity-100 group-hover:drop-shadow-md space-y-2`}>
                             <div className="drop-shadow-md">
                                 {item.icon}
                             </div>
