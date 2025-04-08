bbe82e3ad852247dee9d35d4073f74c5a64387cb
Chatan Petry <95079984+Jesselpetry@users.noreply.github.com>
2025-04-08 19:17:42 +0700

    ALL

--- src/app/setup-profile/page.tsx
+++ src/app/setup-profile/page.tsx
@@ -87,7 +87,7 @@ export default function SetupProfile() {
   }
 
   return (
-    <div className="min-h-screen p-4 flex flex-col relative">
+    <div className="min-h-screen p-4 flex flex-col relative overflow-hidden">
       {/* Background with gradient */}
       <div className="fixed top-0 left-0 w-full h-full bg-gradient-to-br from-[#0E653B] to-[#0C2A20] z-0"></div>
       
@@ -96,34 +96,37 @@ export default function SetupProfile() {
       
       {/* Content */}
       <div className="relative z-10 flex-1">
-        <h1 className="text-3xl font-bold text-white text-center my-6">สร้างบัญชี</h1>
+        <div className="text-center my-6">
+          <h1 className="text-3xl font-bold text-white">สร้างบัญชี</h1>
+            <h2 className="text-xl font-light text-white mt-2 opacity-60">Creating an account</h2>
+        </div>
 
         <div className="space-y-6 mt-2">
-          <div className="space-y-1">
-            <label className="text-white text-xl">ชื่อผู้ใช้</label>
-            <input
-              type="text"
-              placeholder="ชื่อผู้ใช้"
-              className="w-full p-4 rounded-lg bg-black/25 backdrop-blur-sm border border-white/30 text-white text-lg"
-              value={userName}
-              onChange={(e) => setUserName(e.target.value)}
-            />
-          </div>
+        <div className="space-y-2">
+          <label className="text-white text-xl mb-2 block font-medium">ชื่อผู้ใช้</label>
+          <input
+            type="text"
+            placeholder="ชื่อผู้ใช้"
+            className="w-full p-4 rounded-lg bg-black/25 backdrop-blur-sm border border-white/30 text-white text-lg font-light"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </div>
 
           <div className="space-y-1">
-            <label className="text-white text-xl">ชื่อเล่น</label>
+            <label className="text-white text-xl mb-2 block">ชื่อเล่น</label>
             <input
               type="text"
               placeholder="ชื่อเล่น"
-              className="w-full p-4 rounded-lg bg-black/25 backdrop-blur-sm border border-white/30 text-white text-lg"
+              className="w-full p-4 rounded-lg bg-black/25 backdrop-blur-sm border border-white/30 text-white text-lg font-light"
               />
           </div>
 
           <div className="flex space-x-4">
             <div className="flex-1 space-y-1">
-              <label className="text-white text-xl">ยุวชน ปี</label>
+              <label className="text-white text-xl mb-2 block">ยุวชน ปี</label>
               <select
-                className="w-full p-4 rounded-lg bg-black/25 backdrop-blur-sm border border-white/30 text-white text-lg"
+                className="w-full p-4 rounded-lg bg-black/25 backdrop-blur-sm border border-white/30 text-white text-lg font-light"
                 value={year || ''}
                 onChange={(e) => setYear(parseInt(e.target.value))}
               >
@@ -137,25 +140,99 @@ export default function SetupProfile() {
             </div>
 
             <div className="flex-1 space-y-1">
-              <label className="text-white text-xl">จังหวัด</label>
-              <select
-                className="w-full p-4 rounded-lg bg-black/25 backdrop-blur-sm border border-white/30 text-white text-lg"
+              <label className="text-white text-xl mb-2 block">จังหวัด</label>
+                <select
+                className="w-full p-4 rounded-lg bg-black/25 backdrop-blur-sm border border-white/30 text-white text-lg font-light"
                 value={province}
                 onChange={(e) => setProvince(e.target.value)}
-              >
-                <option value="">กรุงเทพ</option>
-                <option value="กรุงเทพ">กรุงเทพ</option>
-                <option value="เชียงใหม่">เชียงใหม่</option>
+                >
+                <option value="">เลือกจังหวัด</option>
+                <option value="กรุงเทพมหานคร">กรุงเทพมหานคร</option>
+                <option value="กระบี่">กระบี่</option>
+                <option value="กาญจนบุรี">กาญจนบุรี</option>
+                <option value="กาฬสินธุ์">กาฬสินธุ์</option>
+                <option value="กำแพงเพชร">กำแพงเพชร</option>
                 <option value="ขอนแก่น">ขอนแก่น</option>
+                <option value="จันทบุรี">จันทบุรี</option>
+                <option value="ฉะเชิงเทรา">ฉะเชิงเทรา</option>
+                <option value="ชลบุรี">ชลบุรี</option>
+                <option value="ชัยนาท">ชัยนาท</option>
+                <option value="ชัยภูมิ">ชัยภูมิ</option>
+                <option value="ชุมพร">ชุมพร</option>
+                <option value="เชียงราย">เชียงราย</option>
+                <option value="เชียงใหม่">เชียงใหม่</option>
+                <option value="ตรัง">ตรัง</option>
+                <option value="ตราด">ตราด</option>
+                <option value="ตาก">ตาก</option>
+                <option value="นครนายก">นครนายก</option>
+                <option value="นครปฐม">นครปฐม</option>
+                <option value="นครพนม">นครพนม</option>
+                <option value="นครราชสีมา">นครราชสีมา</option>
+                <option value="นครศรีธรรมราช">นครศรีธรรมราช</option>
+                <option value="นครสวรรค์">นครสวรรค์</option>
+                <option value="นนทบุรี">นนทบุรี</option>
+                <option value="นราธิวาส">นราธิวาส</option>
+                <option value="น่าน">น่าน</option>
+                <option value="บึงกาฬ">บึงกาฬ</option>
+                <option value="บุรีรัมย์">บุรีรัมย์</option>
+                <option value="ปทุมธานี">ปทุมธานี</option>
+                <option value="ประจวบคีรีขันธ์">ประจวบคีรีขันธ์</option>
+                <option value="ปราจีนบุรี">ปราจีนบุรี</option>
+                <option value="ปัตตานี">ปัตตานี</option>
+                <option value="พระนครศรีอยุธยา">พระนครศรีอยุธยา</option>
+                <option value="พังงา">พังงา</option>
+                <option value="พัทลุง">พัทลุง</option>
+                <option value="พิจิตร">พิจิตร</option>
+                <option value="พิษณุโลก">พิษณุโลก</option>
+                <option value="เพชรบุรี">เพชรบุรี</option>
+                <option value="เพชรบูรณ์">เพชรบูรณ์</option>
+                <option value="แพร่">แพร่</option>
+                <option value="พะเยา">พะเยา</option>
                 <option value="ภูเก็ต">ภูเก็ต</option>
-                {/* Add more provinces as needed */}
-              </select>
+                <option value="มหาสารคาม">มหาสารคาม</option>
+                <option value="มุกดาหาร">มุกดาหาร</option>
+                <option value="แม่ฮ่องสอน">แม่ฮ่องสอน</option>
+                <option value="ยโสธร">ยโสธร</option>
+                <option value="ยะลา">ยะลา</option>
+                <option value="ร้อยเอ็ด">ร้อยเอ็ด</option>
+                <option value="ระนอง">ระนอง</option>
+                <option value="ระยอง">ระยอง</option>
+                <option value="ราชบุรี">ราชบุรี</option>
+                <option value="ลพบุรี">ลพบุรี</option>
+                <option value="ลำปาง">ลำปาง</option>
+                <option value="ลำพูน">ลำพูน</option>
+                <option value="เลย">เลย</option>
+                <option value="ศรีสะเกษ">ศรีสะเกษ</option>
+                <option value="สกลนคร">สกลนคร</option>
+                <option value="สงขลา">สงขลา</option>
+                <option value="สตูล">สตูล</option>
+                <option value="สมุทรปราการ">สมุทรปราการ</option>
+                <option value="สมุทรสงคราม">สมุทรสงคราม</option>
+                <option value="สมุทรสาคร">สมุทรสาคร</option>
+                <option value="สระแก้ว">สระแก้ว</option>
+                <option value="สระบุรี">สระบุรี</option>
+                <option value="สิงห์บุรี">สิงห์บุรี</option>
+                <option value="สุโขทัย">สุโขทัย</option>
+                <option value="สุพรรณบุรี">สุพรรณบุรี</option>
+                <option value="สุราษฎร์ธานี">สุราษฎร์ธานี</option>
+                <option value="สุรินทร์">สุรินทร์</option>
+                <option value="หนองคาย">หนองคาย</option>
+                <option value="หนองบัวลำภู">หนองบัวลำภู</option>
+                <option value="อ่างทอง">อ่างทอง</option>
+                <option value="อุดรธานี">อุดรธานี</option>
+                <option value="อุทัยธานี">อุทัยธานี</option>
+                <option value="อุตรดิตถ์">อุตรดิตถ์</option>
+                <option value="อุบลราชธานี">อุบลราชธานี</option>
+                <option value="อำนาจเจริญ">อำนาจเจริญ</option>
+                </select>
             </div>
           </div>
 
           <div className="space-y-1">
-            <label className="text-white text-xl">รูปโปรไฟล์ <span className="text-sm">*แนะนำให้ใช้รูปขนาด 1:1</span></label>
-            <div className="w-full p-4 rounded-lg bg-black/25 backdrop-blur-sm border border-white/30 text-white text-lg"            >
+            <label className="text-white text-xl mb-2 block">
+              รูปโปรไฟล์ <span className="text-sm font-light">*แนะนำให้ใช้รูปขนาด 1:1</span>
+            </label>
+            <div className="w-full p-4 rounded-lg bg-black/25 backdrop-blur-sm border border-white/30 text-white text-lg font-light"            >
               <label htmlFor="file-upload" className="cursor-pointer">
                 <div className="flex flex-col items-center">
                   <svg className="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
