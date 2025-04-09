aa0f78b41e5655657edfdd506df8fce57822d633
Chatan Petry <95079984+Jesselpetry@users.noreply.github.com>
2025-04-09 23:08:04 +0700

    Update page.tsx

--- src/app/family/page.tsx
+++ src/app/family/page.tsx
@@ -22,7 +22,7 @@ function FamilyPage() {
       <section className="">
         {/* Year Group 68 */}
         <h3 className="text-2xl font-medium mt-6">ยุวชนปี 68</h3>
-        <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-2 mt-4">
+        <div className="grid grid-cols-3 gap-2 mt-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
           {Array(4).fill(null).map((_, index) => (
             <div
               key={index}
