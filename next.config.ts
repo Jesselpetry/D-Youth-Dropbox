4350a8323e2af15cb257bd1b06255e36a329e200
Chatan Petry <95079984+Jesselpetry@users.noreply.github.com>
2025-04-11 03:43:45 +0700

    Deploy error fix

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true, // เปิดใช้ Server Actions (ถ้าคุณใช้)
-  },
   images: {
-    domains: ['axdoxguzojpdpopmflfu.supabase.co',
+    domains: [
+      'axdoxguzojpdpopmflfu.supabase.co',
       'picsum.photos',
     ],
-
   },
 }
 
