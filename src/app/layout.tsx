1e8459c9ae36fec5ecc00bc21c54d9f649f75f60
Chatan Petry <95079984+Jesselpetry@users.noreply.github.com>
2025-04-12 04:37:04 +0700

    Font NEXT

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 // ✅ app/layout.tsx
 import "./globals.css";
 import { ReactNode } from "react";
-
+import localFont from 'next/font/local'
 import Menu from "@/app/components/Menu";
 
 export const metadata = {
@@ -9,10 +9,31 @@ export const metadata = {
   description: "โพสต์และดูข้อความของคุณแบบง่าย ๆ",
 };
 
+const myFont = localFont({
+  src: [
+    {
+      path: '/fonts/anakotmai-light.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '/fonts/anakotmai-medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '/fonts/anakotmai-bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+});
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="th">
-      <body className="bg-gray-50 text-white font-anakotmai">
+      <body className={`${myFont.className} bg-gray-50 text-white`}> 
         
         <Menu /> 
         <main className="max-w-2xl mx-auto px-4 py-6 pb-32">{children}</main>
@@ -20,6 +41,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           <div className="absolute inset-0 bg-gradient-to-br from-[#0E653B] to-[#0C2A20]" />
           <div className="absolute inset-0 bg-grid-pattern opacity-25" />
         </div>
+
       </body>
     </html>
   );
